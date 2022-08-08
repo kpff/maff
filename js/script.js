e74f864b6d1390ee5b2c8ab257237ce5676d6734
Bedrock <108615906+maffakagd@users.noreply.github.com>
2022-08-08 17:23:55 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+	document.getElementById('body').classList.add("class");
+	document.getElementById('body2').classList.add("class");
+	document.getElementById('body3').classList.add("class");
+}
